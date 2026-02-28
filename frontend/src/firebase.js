6ec4ad65fdb0130d95efa5830bb299411e6e519f
Martin <chetinchog@yahoo.com>
2026-02-28 16:54:58 -0300

    security: refactor firebase config to use environment variables

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc, onSnapshot } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA0xDczfRxVE7QbVKQpm2EkM8pleF52KOk",
-    authDomain: "jdsa-ictg.firebaseapp.com",
-    projectId: "jdsa-ictg",
-    storageBucket: "jdsa-ictg.firebasestorage.app",
-    messagingSenderId: "775589919465",
-    appId: "1:775589919465:web:64fca7c0deab9f001dce78",
-    measurementId: "G-2E3XBLX0CJ"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
